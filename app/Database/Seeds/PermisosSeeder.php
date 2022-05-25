368658b401a1dd7def1cdabbd8662568953fb393
Anthony Urrego <antho.120@hotmail.com>
2022-05-24 23:23:12 -0500

    Se orgniaza seed con los permisos

--- app/Database/Seeds/PermisosSeeder.php
+++ app/Database/Seeds/PermisosSeeder.php
@@ -6,17 +6,17 @@ use CodeIgniter\Database\Seeder;
 
 class PermisosSeeder extends Seeder
 {
-    public function run()
-    {
-        $permisos = [1, 11, 12, 13, 14, 15, 2, 21, 22, 23, 24, 3, 31, 32, 33, 4, 41, 42, 43, 5, 51, 52, 53, 6, 7, 8, 81, 82, 83, 84, 85, 86];
-        $datos = [];
-        foreach ($permisos as $key => $value) {
-            $datos[] = [
-                "usuarioId" => 1,
-                "perfilId" => 1,
-                "permiso" => $value
-            ];
-        }
+	public function run()
+	{
+		$permisos = [1, 11, 12, 13, 14, 15, 2, 21, 22, 23, 24, 3, 31, 32, 33, 4, 41, 42, 43, 5, 51, 52, 53, 6, 7, 71, 8, 81, 82, 83, 84, 85, 86];
+		$datos = [];
+		foreach ($permisos as $key => $value) {
+			$datos[] = [
+				"usuarioId" => 1,
+				"perfilId" => 1,
+				"permiso" => $value
+			];
+		}
 		$this->db->table('permisosusuarioperfil')->insertBatch($datos);
-    }
+	}
 }
