eebb6b9c02ce5acf222866c0ebe6f81803521c8d
Anthony Urrego <antho.120@hotmail.com>
2022-05-24 23:34:09 -0500

    Se actualiza manifiestos

--- app/Database/Migrations/2022-04-28-102559_Manifiestos.php
+++ app/Database/Migrations/2022-04-28-102559_Manifiestos.php
@@ -4,40 +4,36 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class Manifiestos extends Migration
-{
-    public function up()
-    {
-      $this->forge->addField([
-				'id'   => [
-					'type'           => 'INT',
-					'constraint'     => 11,
-					'unsigned'       => true,
-					'auto_increment' => true,
-				],
-				'nombre' => [
-					'type'           => 'VARCHAR',
-					'constraint'     => 255,
-				],
-				'estado' => [
-					'type'           => 'TINYINT',
-					'constraint'     => 1,
-					'default'        => 1
-				],
-				'ruta_archivo' => [
-					'type'           => 'VARCHAR',
-					'constraint'     => 255,
-				],
-				'created_at datetime default current_timestamp',
-				'updated_at datetime default current_timestamp on update current_timestamp'
-			]);
+class Manifiestos extends Migration {
+	public function up() {
+		$this->forge->addField([
+			'id'   => [
+				'type'           => 'INT',
+				'constraint'     => 11,
+				'unsigned'       => true,
+				'auto_increment' => true,
+			],
+			'nombre' => [
+				'type'           => 'VARCHAR',
+				'constraint'     => 255,
+			],
+			'estado' => [
+				'type'           => 'TINYINT',
+				'constraint'     => 1,
+				'default'        => 1
+			],
+			'ruta_archivo' => [
+				'type'           => 'TEXT',
+			],
+			'created_at datetime default current_timestamp',
+			'updated_at datetime default current_timestamp on update current_timestamp'
+		]);
 
-			$this->forge->addKey('id', true);
-			$this->forge->createTable('manifiestos');
-    }
+		$this->forge->addKey('id', true);
+		$this->forge->createTable('manifiestos');
+	}
 
-    public function down()
-    {
-      $this->forge->dropTable('manifiestos');
-    }
+	public function down() {
+		$this->forge->dropTable('manifiestos');
+	}
 }
