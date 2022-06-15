9b0334793667d66c04b926283a879df44525f0f4
Anthony Urrego <antho.120@hotmail.com>
2022-06-14 22:31:43 -0500

    Se organizan algunas migraciones para el rollback

--- app/Database/Migrations/2022-06-12-161954_AlmacenUsuario.php
+++ app/Database/Migrations/2022-06-12-161954_AlmacenUsuario.php
@@ -4,24 +4,22 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class AlmacenUsuario extends Migration
-{
-    public function up()
-    {
-        $addFields = [
-            'id_almacen' => [
+class AlmacenUsuario extends Migration {
+	public function up() {
+		$addFields = [
+			'id_almacen' => [
 				'type'           => 'INT',
 				'constraint'     => 11,
 				'null'		       => true,
 				'unsigned'       => true
 			],
-            'CONSTRAINT usuarios FOREIGN KEY(`id_almacen`) REFERENCES `almacenes`(`id`)'
-        ];
-        $this->forge->addColumn('usuarios', $addFields);
-    }
+			'CONSTRAINT usuarios_almacenes_foreign FOREIGN KEY(`id_almacen`) REFERENCES `almacenes`(`id`)'
+		];
+		$this->forge->addColumn('usuarios', $addFields);
+	}
 
-    public function down()
-    {
-        //
-    }
+	public function down() {
+		$this->forge->dropForeignKey('usuarios', 'usuarios_almacenes_foreign');
+		$this->forge->dropColumn("usuarios", "id_almacen");
+	}
 }
