c9ed6f546cf5094d5075580fdf56cfe71ed578dd
Anthony Urrego <antho.120@hotmail.com>
2022-06-18 23:44:11 -0500

    Cambio en la parte de tabulación y ordenamiento del codigo

--- app/Database/Seeds/PaisesSeeder.php
+++ app/Database/Seeds/PaisesSeeder.php
@@ -4,16 +4,14 @@ namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
 
-class PaisesSeeder extends Seeder
-{
-    public function run()
-    {
-        $datos = [
+class PaisesSeeder extends Seeder {
+  public function run() {
+		$datos = [
 			[
 				"nombre" => "Colombia",
 				"estado" => 1,
 			]
 		];
 		$this->db->table('paises')->insertBatch($datos);
-    }
+  }
 }
