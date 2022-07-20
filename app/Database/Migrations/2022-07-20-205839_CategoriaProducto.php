19ff5c44939e2aefd68fb79dbdc431095d452a41
AlejoHurtatis14 <neideralejandro.h@gmail.com>
2022-07-20 18:57:04 -0500

    Se valida crear productos con categoria null.

---
+++ app/Database/Migrations/2022-07-20-205839_CategoriaProducto.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CategoriaProducto extends Migration
+{
+    public function up()
+    {
+        $fields = [
+			'id_categoria' => [
+				'name'           => 'id_categoria',
+				'type'           => 'INT',
+				'constraint'     => 11,
+				'unsigned'       => true,
+				'null'           => true
+			],
+		];
+		$this->forge->modifyColumn('productos', $fields);
+    }
+
+    public function down()
+    {
+        //
+    }
+}
