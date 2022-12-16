631211f44a762a1a7205d79c65c74a0ffaccf5fd
AlejoHurtatis14 <neideralejandro.h@gmail.com>
2022-12-15 22:07:36 -0500

    Se valida proceso de QR para la factura y descarga e imagen en empaque.

---
+++ app/Database/Migrations/2022-12-06-024958_EstadoQRFactura.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class EstadoQRFactura extends Migration
+{
+    public function up()
+    {
+        $addFields = [
+			'leidoQR' => [
+				'type'          => 'TINYINT',
+				'constraint'    => 1,
+                'default'       => 0
+			],
+		];
+		$this->forge->addColumn('ventas', $addFields);
+    }
+
+    public function down()
+    {
+        $this->forge->dropColumn("ventas", "leidoQR");
+    }
+}
