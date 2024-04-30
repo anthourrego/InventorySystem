076ea46d270bec1c6801d39a93273092c69325e0
Anthony Urrego <anthourrego@gmail.com>
2024-04-29 23:13:49 -0500

    Se sube la version 4.4.4

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -41,8 +41,7 @@ class Autoload extends AutoloadConfig
      *       'App'         => APPPATH
      *   ];
      *
-     * @var array<string, array<int, string>|string>
-     * @phpstan-var array<string, string|list<string>>
+     * @var array<string, list<string>|string>
      */
     public $psr4 = [
         APP_NAMESPACE => APPPATH, // For custom app namespace
@@ -86,8 +85,7 @@ class Autoload extends AutoloadConfig
      *       '/path/to/my/file.php',
      *   ];
      *
-     * @var string[]
-     * @phpstan-var list<string>
+     * @var list<string>
      */
     public $files = [
         'app/Helpers/404.php'
@@ -105,8 +103,7 @@ class Autoload extends AutoloadConfig
      *       'form',
      *   ];
      *
-     * @var string[]
-     * @phpstan-var list<string>
+     * @var list<string>
      */
     public $helpers = [];
 }
